dbb50773e70f0ae4007bf7ec2a61d5ae873cbb8b
Yoni Yirmiyahu <48987715+yoniyir@users.noreply.github.com>
2023-05-12 10:44:05 +0300

    openssl

--- app/__init__.py
+++ app/__init__.py
@@ -7,8 +7,6 @@ from config import Config
 from flask_login import LoginManager
 from flask_login import UserMixin
 
-from werkzeug.security import generate_password_hash, check_password_hash
-
 login = LoginManager()
 
 db = SQLAlchemy()
@@ -16,6 +14,7 @@ migrate = Migrate()
 
 def create_app(config_class=Config):
     app = Flask(__name__)
+   
     app.config.from_object(config_class)
 
     db.init_app(app)
@@ -30,28 +29,3 @@ def create_app(config_class=Config):
 
     return app
 
-from app import models
-"""
-class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    __table_args__ = {'extend_existing': True}
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    last_seen = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-    def check_password(self, password):
-        return check_password_hash(self.password_hash, password)
-
-    @login.user_loader
-    def load_user(id):
-        return User.query.get(int(id))
-
-    """
